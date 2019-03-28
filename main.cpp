7df497d0a1ce88d70316a0c364ef2c9846d3d3cf
Myles <winnerkd288@gmail.com>
2019-03-28 05:18:27 -0400

    Fixed the beginning bug, all that is left is for the path functions to find the key

--- main.cpp
+++ main.cpp
@@ -8,25 +8,22 @@ std::string name;
 
 std::string beginning() //The beginning path should start the game and lead to the seperate files with their own paths but lead back to home hopefully after finding the key.
 	{
-    int path;
-    std::cout << "Here are your paths to find the keys, what will you choose, where could they be?\n\n";
-		std::cout << "1)  \n"; // Path for Myles
-		std::cout << "2)  \n"; // Path Fabian
-		std::cout << "3)  \n"; // Path for Marcos
-		std::cin >> path;
-		if (path == 1) 
+    int path = 0;
+    std::cout << "\nHere are your paths to find the keys, what will you choose, where could they be?\n\n";
+		std::cout << "1) Myles path \n"; // Path for Myles
+		std::cout << "2) Fabian path \n"; // Path Fabian
+		std::cout << "3) Marcos path \n"; // Path for Marcos
+		while(path != 1 || path != 2 || path != 3) 
 		{
-      std::cout << "Going to Myles path\n"; // Name should be changed after path is created
-		  //Path
-		} else if (path == 2) 
-		{
-      std::cout << "Going to Fabian path\n"; // Name should be changed after path is created
-		  //Path
-		} else if (path == 3) {
-      std::cout << "Going to Marcos path\n"; // Name should be changed after path is created
-		  //Path
-		} else {
-			std::cout << "Invalid try again.\n";
+			std::cin >> path;
+			if(path == 1 || path == 2 || path == 3) {
+				std::cout << "Path taken\n";
+				return 0;
+			} else {
+				std::cin.clear();
+        std::cin.ignore();
+        std::cout << "Invalid try again.\n";
+			}
 		}
 		return 0;
 	}
@@ -37,11 +34,11 @@ std::string home() //Home function. Every path that ends should lead here
 		std::cout << "=======================\n";
     std::cout << "Do you have the 3 keys?\n";
 		std::cout << "=======================\n";
-		std::cout << "Key 1: ";
+		std::cout << "Key 1: \n";
 		std::cin >> key1;
-		std::cout << "\nKey 2: ";
+		std::cout << "Key 2: \n";
 		std::cin >> key2;
-		std::cout << "\nKey 3: ";
+		std::cout << "Key 3: \n";
 		std::cin >> key3;
 
 		if (key1 == 3016 && key2 == 6271 && key3 == 4001) //If all the keys are found run the End game
@@ -53,13 +50,13 @@ std::string home() //Home function. Every path that ends should lead here
 			std::cout << "Congratulations! You have completed the Quest and opened the chest.\n";
 			std::cout << "You are truly magnificent!\n";
 			std::cout << "Inside this chest is a key, a key to a kingdom that belongs to you\n";
-			std::cout << "All hail " + name + "the Magnificent you are truly great\n";
+			std::cout << "All hail " + name + " the Magnificent you are truly great\n";
 			std::cout << "End of letter\n";
 			std::cout << "=========================\n";
-			std::cout << "Here is you Kingdom rule with great Authority!\n";
+			std::cout << "Here is your Kingdom rule with great Authority!\n";
 			std::cout << ""; //Temporary will put a kingdom of characters
 		} else { //If the correct keys weren't found they need to keep looking
-			std::cout << "Thats not it keep looking.\n";
+			std::cout << "\nThats not it keep looking.\n";
       beginning();
 		}
 		return 0;
